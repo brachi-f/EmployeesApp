087114239068e2b3ce294991b68558c69a246e53
brachi-f <braf5866@gmail.com>
2024-04-04 19:54:36 +0300

    form is not working

--- src/pages/edit.jsx
+++ src/pages/edit.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { useFieldArray, useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
-import { Form } from 'semantic-ui-react'
+import { Button, Form, Icon } from 'semantic-ui-react'
 import * as yup from 'yup'
 import { useParams } from 'react-router-dom'
 import * as empService from '../services/employees'
-import { TextField } from '@mui/material'
+import { FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material'
 import { useMemo } from 'react'
 
 
 const Edit = () => {
-    const [employee, setEmployee] = useState();
-    const [gender, setGender] = useState(true);
+    const [employee, setEmployee] = useState(null);
     let { id } = useParams();
     useEffect(() => {
         empService.getEmployeeById(id).then(res => {
@@ -19,34 +18,47 @@ const Edit = () => {
         }).catch(err => console.error("Error by getting employee by id: ", err))
     }, [])
 
-    console.log("employee:", employee)
     const empSchema = yup.object({
         firstName: yup.string().required(),
         familyName: yup.string().required(),
         identity: yup.string().min(9).max(9).required(),
         dateOfBirth: yup.date().required(),
         dateStart: yup.date().required(),
-        gender: yup.number().min(0).max(1).required(),
-        // status: yup.bool().default(true),
-        // roles: yup.array().of(
-        //     yup.object().shape({
-        //         name: yup.number().required(),
-        //         management: yup.bool().required(),
-        //         dateStart: yup.date().required()
-        //     })
-        // )
+        gender: yup.number().required(),
+        status: yup.bool().default(true),
+        roles: yup.array().of(
+            yup.object().shape({
+                id: yup.number(),
+                name: yup.number().required(),
+                management: yup.bool().required(),
+                dateStart: yup.date().required()
+            })
+        )
     })
-    const { register, handleSubmit, reset, formState = { errors }, control } = useForm({
+   
+    const { register, handleSubmit, reset, formState = { errors }, control, getValues } = useForm({
         resolver: yupResolver(empSchema),
-        defaultValues: useMemo(() =>
-            employee
+        defaultValues: useMemo(() => {
+            if (employee) {
+                let item = employee
+                employee.dateOfBirth = new Date(employee.dateOfBirth).toISOString().substring(0, 10)
+                employee.dateStart = new Date(employee.dateStart).toISOString().substring(0, 10)
+                return item
+            }
+            return employee
+        }
             , [employee])
     })
+    const { fields: RolesFields, append: RolesAppend, remove: RolesRemove } = useFieldArray({
+        control,
+        name: "roles",
+    });
     const send = (data) => {
         console.log(data)
     }
     useEffect(() => {
         reset(employee);
+        employee?.roles?.map((r) => RolesAppend(r))
     }, [employee]);
     return (
         <Form onSubmit={handleSubmit(send)}>
@@ -60,7 +72,6 @@ const Edit = () => {
                 fullWidth
             />
             <TextField
-
                 label='Family Name'
                 {...register("familyName")}
                 variant='standard'
@@ -68,6 +79,56 @@ const Edit = () => {
                 margin='dense'
                 fullWidth
             />
+            <TextField
+                label='ID number'
+                {...register("identity")}
+                variant='standard'
+                type='text'
+                margin='dense'
+                fullWidth
+            />
+            <TextField
+                label='Birth date'
+                {...register("dateOfBirth")}
+                variant='standard'
+                type='date'
+                margin='dense'
+                fullWidth
+            />
+            <TextField
+                label='Start date'
+                {...register("dateStart")}
+                variant='standard'
+                type='date'
+                margin='dense'
+                fullWidth
+            />
+            <Select
+                label='Gender'
+                //select
+                {...register('gender')}
+                //  value={employee == null? '2' : employee.gender.toString()}
+                fullWidth
+                defaultValue={employee ? employee.gender : '2'}
+            >
+                <MenuItem value='2' disabled /* selected={!employee}*/>
+                    <Icon name='female' />|
+                    <Icon name='male' />
+                    Gender</MenuItem>
+                <MenuItem value='0' /*selected={employee && employee.gender == 0}*/>
+                    <Icon name='female' />
+                    Female</MenuItem>
+                <MenuItem value='1' >
+                    <Icon name='male' /*selected={employee && employee.gender == 1}*/ />
+                    Male</MenuItem>
+            </Select>
+            <h4>Roles</h4>
+            {RolesFields.map((r) => <>
+                <p>{r.name}</p>
+            </>)}
+            <Button onClick={() => {
+                console.log("values", getValues())
+            }}>do</Button>
         </Form>
     )
 
